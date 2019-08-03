23c11b237a8c5561d01ed4175e1c857c6333828c
Rubrob <stas210698@gmail.com>
2019-08-03 23:10:18 +0000

    update

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express    = require('express')
 const bodyParser = require('body-parser')
 const morgan     = require('morgan')
 const mongoose   = require('mongoose')
+const path = require('path');
 require('dotenv').config();
 
 // DB initialization
@@ -21,19 +22,21 @@ mongoose.connect(mongoURI, {
 
 const app = express()
 
+
 // Middlewares
 if(!process.env.NODE_ENV === 'test'){
     app.use(morgan('dev'))
 }
 
 app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Routes
 app.use('/users', require('./routes/users'))
 app.use('/', require('./routes/orders'))
 app.use('/', require('./routes/products'))
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
--- server/build/service-worker.js
+++ server/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.752b0a14ecde28354144cd4150bddbe0.js"
+  "/precache-manifest.6e94bfe79f347a09f55cd994234930b8.js"
 );
 
 self.addEventListener('message', (event) => {
