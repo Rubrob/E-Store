cd9de28c0e00d213d4d087096a67b9a9cceaf79d
Rubrob <stas210698@gmail.com>
2019-08-05 19:52:38 +0000

    update_5.8.19

--- server/build/service-worker.js
+++ server/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f304b9ba57953df6f6803ba3165e1666.js"
+  "/precache-manifest.1cbdba4bca69c51485635fe232afe9fe.js"
 );
 
 self.addEventListener('message', (event) => {
