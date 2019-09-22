9cb78481621a65d6385e8220fac4d0726517550b
Rubrob <32711205+Rubrob@users.noreply.github.com>
2019-09-22 06:27:25 +0300

    fix

---
+++ server/build/precache-manifest.23655a30c2e11bb9d9b0d463281312a5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af9be4a35b2e6d475a0b265b201c373",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1408b6751b6351e04fb",
+    "url": "/static/css/2.f586341b.chunk.css"
+  },
+  {
+    "revision": "4e0b9e7b32f215683609",
+    "url": "/static/css/main.fb3f0385.chunk.css"
+  },
+  {
+    "revision": "b1408b6751b6351e04fb",
+    "url": "/static/js/2.ffe3c915.chunk.js"
+  },
+  {
+    "revision": "4e0b9e7b32f215683609",
+    "url": "/static/js/main.168e70a4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
