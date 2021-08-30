ac3edd13a555b3b278cdd036b13d35c81add82c3
Stas Lysak <staslyzak@gmail.com>
2021-08-30 16:19:05 +0300

    minor fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,107 +1,117 @@
-import React, {useEffect} from "react";
-import {compose} from "redux";
-import {connect} from "react-redux";
-import {Route, Redirect} from "react-router-dom";
-import ProductList from "containers/ProductList";
-import ProductPage from "containers/ProductList/ProductPage";
-import {
-  filterURL,
-  filterProductsWithURL,
-  fetchProducts,
-} from "redux/actions/products";
-import {fetchMember} from "redux/actions/auth";
-import Loader from "components/Loader";
-import MainBlock from "components/MainBlock";
-import Loadable from "react-loadable";
-import {withRouter} from "react-router-dom";
-
+import React, { useEffect } from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+import ProductList from 'containers/ProductList';
+import ProductPage from 'containers/ProductList/ProductPage';
+import { filterProductsWithURL, fetchProducts } from 'redux/actions/products';
+import { fetchMember } from 'redux/actions/auth';
+import Loader from 'components/Loader';
+import MainBlock from 'components/MainBlock';
+import Loadable from 'react-loadable';
+import { withRouter } from 'react-router-dom';
 
 const createLoadableComponent = (pathResolver) => {
-  return Loadable({
-    loader: pathResolver,
-    loading: props =>
-      props.error ? console.error(props.error) : <Loader />
-  });
-}
-
+    return Loadable({
+        loader: pathResolver,
+        loading: (props) =>
+            props.error ? console.error(props.error) : <Loader />,
+    });
+};
 
 const App = ({
-  fetchMember,
-  fetchProducts,
-  isFetching,
-  filterProductsWithURL,
-  isAuthenticated,
-  ...props
+    fetchMember,
+    fetchProducts,
+    isFetching,
+    filterProductsWithURL,
+    isAuthenticated,
+    ...props
 }) => {
-  useEffect(() => {
-    fetchMember()
-    fetchProducts()
-  },[fetchMember, fetchProducts])
+    useEffect(() => {
+        fetchMember();
+        fetchProducts();
+    }, [fetchMember, fetchProducts]);
 
-  const routes = [
-    {
-      exact: true,
-      path: "/",
-      component: createLoadableComponent(() => import("./components/FrontPage"))
-    },
-    {
-      path: "/profile",
-      component: createLoadableComponent(() => import("./containers/Profile"))
-    },
-    {
-      exact: true,
-      path: "/cart",
-      component: createLoadableComponent(() => import("./containers/Cart"))
-    },
-    {
-      exact: true,
-      path: "/checkout",
-      component: createLoadableComponent(() => import("./containers/Checkout"))
-    },
-    {
-      exact: true,
-      path: "/p",
-      component: createLoadableComponent(() => import("./containers/ProductList"))
-    },
-    {
-      exact: true,
-      path: "/register",
-      component: createLoadableComponent(() => import("./containers/SingUpIn"))
+    const routes = [
+        {
+            exact: true,
+            path: '/',
+            component: createLoadableComponent(() =>
+                import('./components/FrontPage')
+            ),
+        },
+        {
+            path: '/profile',
+            component: createLoadableComponent(() =>
+                import('./containers/Profile')
+            ),
+        },
+        {
+            exact: true,
+            path: '/cart',
+            component: createLoadableComponent(() =>
+                import('./containers/Cart')
+            ),
+        },
+        {
+            exact: true,
+            path: '/checkout',
+            component: createLoadableComponent(() =>
+                import('./containers/Checkout')
+            ),
+        },
+        {
+            exact: true,
+            path: '/p',
+            component: createLoadableComponent(() =>
+                import('./containers/ProductList')
+            ),
+        },
+        {
+            exact: true,
+            path: '/register',
+            component: createLoadableComponent(() =>
+                import('./containers/SingUpIn')
+            ),
+        },
+    ];
+
+    if (props.location.pathname.indexOf('/profile') >= 0 && !isAuthenticated) {
+        return <Redirect to='/register' />;
     }
-  ]
 
-  if(props.location.pathname.indexOf("/profile") >= 0 && !isAuthenticated) {
-    return  <Redirect to="/register" />
-  }
-
-  return (
-    <MainBlock isCartLocation={props.location.pathname === "/cart"}>
-      {routes.map((routeprops) => (
-        <Route key={routeprops.path} {...routeprops} />
-      ))}
-      <Route exact path="/pp/:productId/:colorId" component={ProductPage} />
-      <Route exact path="/p/:filter" render={({match}) => {
-          const URL = filterURL(match.params.filter, "-", 2)
-          filterProductsWithURL(URL)
-          return <ProductList title={URL} />
-        }}
-      />
-    </MainBlock>
-  );
-}
+    return (
+        <MainBlock isCartLocation={props.location.pathname === '/cart'}>
+            {routes.map((routeprops) => (
+                <Route key={routeprops.path} {...routeprops} />
+            ))}
+            <Route
+                exact
+                path='/pp/:productId/:colorId'
+                component={ProductPage}
+            />
+            <Route
+                exact
+                path='/p/:filter'
+                render={(props) => <ProductList {...props} />}
+            />
+        </MainBlock>
+    );
+};
 
 export default compose(
-  withRouter,
-  connect(
-    (state) => ({
-      products: state.products.products,
-      isFetching: state.products.isFetching,
-      isAuthenticated: state.auth.isAuthenticated,
-    }),
-    (dispatch) => ({
-      filterProductsWithURL: (value) => dispatch(filterProductsWithURL(value)),
-      fetchMember: () => dispatch(fetchMember()),
-      fetchProducts: () => dispatch(fetchProducts()),
-    })
-  )
-)(App)
+    withRouter,
+    connect(
+        (state) => ({
+            products: state.products.products,
+            isFetching: state.products.isFetching,
+            isAuthenticated: state.auth.isAuthenticated,
+        }),
+        (dispatch) => ({
+            filterProductsWithURL: (value) =>
+                dispatch(filterProductsWithURL(value)),
+            fetchMember: () => dispatch(fetchMember()),
+            fetchProducts: () => dispatch(fetchProducts()),
+        })
+    )
+)(App);
--- client/src/containers/ProductList/index.js
+++ client/src/containers/ProductList/index.js
@@ -1,68 +1,77 @@
-import React from "react";
-import "./styles.sass";
-import {connect} from "react-redux";
-import {
-  useMediaQuery,
-  SwipeableDrawer,
-  Typography,
-} from "@material-ui/core";
-import {toggleFilter} from "redux/actions/trigers";
-import SubHeader from "../SubHeader";
-import Filter from "../SubHeader/Filter";
-import ProductCard from "./ProductCard";
-
+import React from 'react';
+import './styles.sass';
+import { connect } from 'react-redux';
+import { useMediaQuery, SwipeableDrawer, Typography } from '@material-ui/core';
+import { filterURL, filterProductsWithURL } from 'redux/actions/products';
+import { toggleFilter } from 'redux/actions/trigers';
+import SubHeader from '../SubHeader';
+import Filter from '../SubHeader/Filter';
+import ProductCard from './ProductCard';
 
 const ProductList = ({
-  filtered,
-  currency,
-  searchedStr,
-  isFilterOpen,
-  toggle
+    match,
+    filtered,
+    currency,
+    searchedStr,
+    isFilterOpen,
+    toggle,
 }) => {
-  const match = useMediaQuery("(max-width: 959.5px)")
-  const productCards = filtered.map(product => <ProductCard key={product._id} product={product} currency={currency} />)
+    const URL = filterURL(match.params.filter, '-', 2);
 
-  return (
-    <div className="productList">
-      <SubHeader />
-      {(match && filtered.length > 0) && <Typography variant="h5" children={searchedStr} />}
-      <div className="flexRow">
-        {match ?
-          <SwipeableDrawer
-            open={isFilterOpen}
-            onClose={toggle}
-            onOpen={toggle}
-            children={<Filter />}
-          /> :
-          <Filter hide={isFilterOpen} />
-        }
-        <div className="productList-content">
-          <div className="productList-content-main">
-            {filtered.length < 1 ?
-              <Typography
-                variant="h5"
-                align="center"
-                className="productList-empty"
-              >
-                I Couldn't Find Anything For Your Request
-              </Typography>
-            : productCards}
-          </div>
+    const matchMedia = useMediaQuery('(max-width: 959.5px)');
+    const productCards = filtered.map((product) => (
+        <ProductCard key={product._id} product={product} currency={currency} />
+    ));
+
+    React.useEffect(() => {
+        filterProductsWithURL(URL);
+    }, [URL]);
+
+    return (
+        <div className='productList'>
+            <SubHeader />
+            {matchMedia && filtered.length > 0 && (
+                <Typography variant='h5' children={searchedStr} />
+            )}
+            <div className='flexRow'>
+                {matchMedia ? (
+                    <SwipeableDrawer
+                        open={isFilterOpen}
+                        onClose={toggle}
+                        onOpen={toggle}
+                        children={<Filter />}
+                    />
+                ) : (
+                    <Filter hide={isFilterOpen} />
+                )}
+                <div className='productList-content'>
+                    <div className='productList-content-main'>
+                        {filtered.length < 1 ? (
+                            <Typography
+                                variant='h5'
+                                align='center'
+                                className='productList-empty'
+                            >
+                                I Couldn't Find Anything For Your Request
+                            </Typography>
+                        ) : (
+                            productCards
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
-}
+    );
+};
 
 export default connect(
-  (state) => ({
-    // filtered: state.products.filtered,
-    filtered: state.products.products,
-    searchedStr: state.products.searchedStr,
-    currency: state.products.currency,
-    isFilterOpen: state.trigers.filter
-  }),
-  (dispatch) => ({
-    toggle: () => dispatch(toggleFilter())
-  })
-)(ProductList)
+    (state) => ({
+        filtered: state.products.filtered,
+        searchedStr: state.products.searchedStr,
+        currency: state.products.currency,
+        isFilterOpen: state.trigers.filter,
+    }),
+    (dispatch) => ({
+        toggle: () => dispatch(toggleFilter()),
+    })
+)(ProductList);
